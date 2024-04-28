c7193bbcff9fb924a27c4b55768f9a863ae8edd7
Pawel_Klosek <pawel.klosek21@gmail.com>
2024-04-28 15:26:45 +0200

    "Notepad simple class"

--- notepad+-.py
+++ notepad+-.py
@@ -1,9 +1,37 @@
 import os
-def otwieranie():
-    user_file = input("Podaj scierzkę do pliku")
-    textfile = open(user_file)
 
-    print(bool(textfile))
+class OpenFile:
+    def road(rd):
+        return os.path.abspath(rd)
+
+    def plik(file):
+        x = open(file)
+        return x.read()
+
+
+
+
+
+def edit_file():
+    user_file = input("Podaj scierzkę do pliku")
+
+    try:
+        user_input_open = OpenFile.plik(user_file)
+
+        if bool(user_input_open) == True:
+
+            print(OpenFile.road(user_file))
+            print(OpenFile.plik(user_file))
+
+
+
+    except:
+        print("Ściszka do pliku jest błędna")
+
+
+
 
 if __name__ == '__main__':
-    otwieranie()
+    edit_file()
+
+
