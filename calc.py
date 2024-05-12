ca63958ecac1e212e56664802362ded6295cb06b
Pawel_Klosek <pawel.klosek21@gmail.com>
2024-05-12 09:42:23 +0200

    zmianu

--- calc.py
+++
@@ -1,4 +0,0 @@
-print("Hello World")
-#1
-for a in range(1,20):
-    print(f"User{a}")
--- file_expolrer.py
+++ file_expolrer.py
@@ -2,31 +2,17 @@ import os
 import subprocess
 
 class OpenFile:
-    @staticmethod
-    def road():
-        return input("Podaj ścieżkę do pliku: ")
+
 
     @staticmethod
     def plik(file):
-        with open(file) as f:
-            return f.read()
+        os.listdir(file)
 
-    @staticmethod
-    #do zastąpienia przez własny edytor w tk
-    def edycja(note_file):
-        subprocess.Popen(['ls'])
 
-def edit_file():
-    try:
-        file_path = OpenFile.road()
-        user_input_open = OpenFile.plik(file_path)
-        print("Otwarto plik:", file_path)
-        print("Zawartość pliku:")
-        print(user_input_open)
-        OpenFile.edycja(file_path)
 
-    except Exception as e:
-        print("Wystąpił błąd:", e)
+        
 
 if __name__ == '__main__':
-    edit_file()
+    innn = input("Podaj ścieżkę do pliku: ")
+
+    print(OpenFile.plik(f'{innn}'))
